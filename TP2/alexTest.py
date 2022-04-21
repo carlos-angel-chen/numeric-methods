852baae7c0133afce030eeab62d5540604185a96
alero <amoldovan@itba.edu.ar>
2022-04-21 02:51:13 -0300

    a la grande le puse cuca

--- TP2/alexTest.py
+++ TP2/alexTest.py
@@ -9,28 +9,35 @@
 # Sí.
 
 # 4) Si ustedes están intentando de usar estimaciones teóricas del error relativo de los métodos numéricos, por favor, lean otras consultas sobre el tema en este mismo foro. 
+# Si se refieren a lo que pide el punto 3 del TP, la función sonido debe devolver el error del ajuste (con signo) para cada punto. En términos de las filminas presentadas en clase, se pide el vector e->
 
 # 5) No imagino para qué quieren usar np.finfo().eps, pero sí pueden.
 
 # 6) pueden usar el producto de matrices y la transpuesta.
 
-#
+#####################
+
+# A) CHEQUEAR QUE TENGA LA FORMA ADECUADA
 
 import numpy as np
 
-A = np.zeros((2,3))
+# A = np.zeros((2,3))
 
-row0 = [1,2,3]
-row1 = [4,5,6]
+# row0 = [1,2,3]
+# row1 = [4,5,6]
 
-A[0] = row0
-A[1] = row1
+# A[0] = row0
+# A[1] = row1
 
-print(A)
+# print(A)
 
-A = np.zeros((441000,6))
+# A = np.zeros((441000,6))
 
-A[0] = [1,2,3,4,5,6]
-A[440998] = [1,2,3,4,5,6]
+# A[0] = [1,2,3,4,5,6]
+# A[440998] = [1,2,3,4,5,6]
 
-print(A)
+# print(A)
+
+y = np.array([[1,2,3,4,5]])
+b = y.T
+print(b)
