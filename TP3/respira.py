a95afc28c7a8af4b53fd423edc4a51688531d0e0
fnmolina <famolina@itba.edu.ar>
2022-05-05 21:26:41 -0300

    listo codigo

--- TP3/respira.py
+++ TP3/respira.py
@@ -2,7 +2,14 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Funcion:   solvercrit
+#           Aproxima valor de rcrit mediante algoritmo de Newton-Raphson
+#           El punto inicial se determina mediante una aproximacion cuadratica a la funcion
+#Recibe:    valor de z.
+#Devuelve:  r critico
 def solvercrit(z):
+
+    #constantes
     Dt = 8e-6
     v = 0.005
     rc = 0.0005
@@ -11,23 +18,26 @@ def solvercrit(z):
     c0 = 5
     r0 = 0.01
 
+    #constantes
     alpha = (4*Dt*c0) / (r0 * (rc + tm)**2)
     beta = (4*Dt) / (v*(rc**2))
     gamma = (2 * Dt) / (rc * k0)
-
-    y = 1       #punto inicial
+    
+    #punto inicial
+    y = np.sqrt((alpha+z*beta+gamma-1)/(-1+gamma+beta*z))       
 
     eps = 10e-9  #cota 
 
+    #calculo de y0
     yexp2 = y**2
     fx = yexp2 * np.log(yexp2) - yexp2 + 1 - alpha + beta * (yexp2 - 1) * z + gamma * (yexp2 - 1)
     dfx = 2*y*(beta*z + gamma + np.log(yexp2))
 
-    y1 = y - fx/dfx
-    n = 0
+    y1 = y - fx/dfx     #primera iteracion
+    n = 1      
 
     while(np.abs(y1 - y) > eps or fx > eps):
-        n += 1      #
+        n += 1      
         y = y1
         yexp2 = y**2
         fx = yexp2 * np.log(yexp2) - yexp2 + 1 - alpha + beta * (yexp2 - 1) * z + gamma * (yexp2 - 1)
@@ -37,12 +47,14 @@ def solvercrit(z):
     rcrit = y1 * (rc + tm)
     return rcrit    
 
+
+#Funcion:   rcritplot
+#           Grafica r critico para valores de z entre 0 y 0.1
 def rcritplot():
     z = np.linspace(0,0.1,1001)
     rcrit = np.empty(len(z))
     for i in range(len(z)):
        rcrit[i] = solvercrit(z[i])
-    print(z[1]-z[0])
 
     plt.title('rcrit(z)')
     plt.xlabel('z')
@@ -51,6 +63,3 @@ def rcritplot():
     plt.plot(z, rcrit)
 
     plt.show()
-    
-
-rcritplot()
