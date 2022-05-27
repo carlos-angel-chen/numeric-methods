a46c561d667d92c1fdc535a256e8a2d0efdfda0d
fnmolina <famolina@itba.edu.ar>
2022-05-26 22:34:57 -0300

    ej2 complete

--- TP4/ej2.py
+++ TP4/ej2.py
@@ -10,37 +10,50 @@ def higginsselkov():
     tf = 600                                               
     h = 0.01                                                
     x0 = np.array([2,3])      #s0,p0
+    v0 = vc = calcVc()
 
-    v0 = calcVc()
-    
+    print(v0)
     t, result = ruku4(dx, t0, tf, h, x0)   
-
     result = result.T
+    plot(t, result[0], result[1], "Higgins-Selkov model with v0 = vc")
 
-    figure, axis = plt.subplots(2, 1)
-    axis[0,0].plot(t, result[0])
-    axis[1,0].plot(t, result[1])
-    plt.title('Higgin-Selkov model')
-    axis[0,0].xlabel('t')
-    axis[1,0].xlabel('t')
-    axis[0,0].ylabel('s')
-    axis[1,0].ylabel('p')
-    axis[0,0].legend()
-    axis[1,0].legend()
-    #plt.grid(color='b', ls = '-.', lw = 0.25)  
-    plt.show()
+    v0 = vc-0.01 
+    print(v0)
+    t, result = ruku4(dx, t0, tf, h, x0)   
+    result = result.T
+    plot(t, result[0], result[1], "Higgins-Selkov model with v0 < vc")
+
+    v0 = vc+0.05 
+    print(v0)
+    t, result = ruku4(dx, t0, tf, h, x0)   
+    result = result.T
+    plot(t, result[0], result[1], "Higgins-Selkov model with v0 > vc")
+
 
 def dx(t,x):
     return np.array([v0 - 0.23 * x[0] * (x[1]**2), 0.23 * x[0] * (x[1]**2) - 0.40 * x[1]])
 
-def isOsc(t0, tf, h, f):
+def plot(t, s, p, title):
+    plt.subplot(2, 1, 1)
+    plt.plot(t, s)
+    plt.ylabel('s')
+    plt.title(title)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(t, p)
+    plt.xlabel('t')
+    plt.ylabel('p')
+
+    plt.show()
+
+def isOsc(t0, tf, h, arr):
     dt = (tf-t0)/h
-    eps = 0.1
+    eps = 0.01
     for i in range(2):
-        min1 = min(f[i][int(dt/2):int(dt*3/4)])
-        min2 = min(f[i][int(dt*3/4):int(dt)])
-        max1 = max(f[i][int(dt/2):int(dt*3/4)])
-        max2 = max(f[i][int(dt*3/4):int(dt)])
+        min1 = min(arr[i][int(dt/2):int(dt*3/4)])
+        min2 = min(arr[i][int(dt*3/4):int(dt)])
+        max1 = max(arr[i][int(dt/2):int(dt*3/4)])
+        max2 = max(arr[i][int(dt*3/4):int(dt)])
         if abs(min2 - min1) > eps or abs(max2 - max1) > eps:
             return False
     return True 
@@ -51,22 +64,24 @@ def calcVc():
     tf = 600                                               
     h = 0.01     
     x0 = np.array([2, 3])
+    global v0
     v0 = vc = 0.48                     
 
-    delta = 0.1
+    delta = 0.01
     for i in range(10):
-        t, v = ruku4(dx, t0, tf, h, x0)  
-        v = v.T                            
-        if isOsc(v, 0, 600, 0.01):                                   
+        t, res = ruku4(dx, t0, tf, h, x0)  
+        res = res.T                          
+        if isOsc(t0, tf, h, res):         
+            print("True")                          
             v0 += delta                                                
             if v0 == vc:  
                 delta /= 2                                             
                 v0 -= delta 
-        else:                                                              
+        else:   
+            print("False")                                                                
             vc = v0
             delta /= 2                                            
             v0 -= delta 
-    print(vc)
     return vc
 
 
