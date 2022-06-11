e6d0feb0e9b4791614f0b46f3b02d7dfb74b5c1b
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-06-11 20:35:32 -0300

    debug

--- TP5/ejer2.py
+++ TP5/ejer2.py
@@ -14,17 +14,19 @@ yi = []
 N = 0
 
 # Coleccion de puntos iniciales a b c T1 T2
-X0 = np.array([ [0,0,0,0,0],
-                [1,0,0,0,0],
-                [0,1,0,0,0],
-                [0,0,1,0,0],
-                [0,0,0,1,0],
-                [0,0,0,0,1]])
+X0 = np.array([ [0,0,0,1,1],
+                [1,0,0,1,1],
+                [0,1,0,1,1],
+                [0,0,1,1,1],
+                [0,0,0,2,1],
+                [0,0,0,1,2]])
+tol = 1e-1
+itmax = 10
 
 parameter = np.array([a,b,c,T1,T2])
 
 def read_temp_file():
-    df = pd.read_csv('temp.txt',header=None,names=['ti','yi'],sep=' ')
+    df = pd.read_csv('TP5\\temp.txt',header=None,names=['ti','yi'],sep=' ')
     ti = np.array(df['ti'].tolist())
     yi = np.array(df['yi'].tolist())
     N = len(ti)
@@ -47,4 +49,11 @@ def eje2_func(parameter):
 def temperatura():
     global ti, yi, N
     ti, yi, N = read_temp_file()
-    param, param_eval = minimi(eje2_func, )
+    param, param_eval, cant_ite = minimi(eje2_func, X0, tol, itmax)
+
+    return param, param_eval
+
+
+param, param_eval = temperatura()
+print(param)
+print(param_eval)
