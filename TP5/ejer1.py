c94263c561aafaf6dfa1d1b23308dbec81c88ce6
alero <amoldovan@itba.edu.ar>
2022-06-11 19:20:27 -0300

    a

--- TP5/ejer1.py
+++ TP5/ejer1.py
@@ -2,7 +2,7 @@ from scipy.optimize import minimize
 import numpy as np
 
 def minimi(func,Xo,tol,itmax):
-    return Xo[:,0]
+    return Xo[:,0],func(Xo[:,0])
 
 # FACU ACA TA LA COSA
 m = minimize(lambda x: (x[0]-3)**2 + (x[1]-2)**2 + 1, np.array([0,0]),method='Nelder-Mead',tol=1e-6,options={'maxiter':1000})
@@ -12,4 +12,8 @@ f = m.fun
 print(x,f)
 
 
+
+def minimi_(func,Xo,tol,itmax):
+    # Xo = []
+    pass
 
