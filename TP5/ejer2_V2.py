244c35dba238cd8ba4aec1bc00b06d8c5cad2a99
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-06-19 15:46:41 -0300

    ejer2_V2

--- TP5/ejer2_V2.py
+++ TP5/ejer2_V2.py
@@ -16,13 +16,19 @@ N = 0
 
 # Coleccion de puntos iniciales a b c T1 T2
 X0 = np.array([10,10,10,11,11])
+# X0 = np.array([ [10,10,10,11,11],
+#                 [11,10,10,11,11],
+#                 [10,11,10,11,11],
+#                 [10,10,11,11,11],
+#                 [10,10,10,12,11],
+#                 [10,10,10,11,12]])
 tol = 1e-12
 itmax = 1000
 
 parameter = np.array([a,b,c,T1,T2])
 
 def read_temp_file():
-    df = pd.read_csv('TP5/temp.txt',header=None,names=['ti','yi'],sep=' ')
+    df = pd.read_csv('temp.txt',header=None,names=['ti','yi'],sep=' ')
     ti = np.array(df['ti'].tolist())
     yi = np.array(df['yi'].tolist())
     N = len(ti)
@@ -45,7 +51,7 @@ def eje2_func(parameter):
 def temperatura():
     global ti, yi, N
     ti, yi, N = read_temp_file()
-    param, param_eval, cant_ite = minimi(eje2_func, None, X0, tol, itmax)
+    param, param_eval, cant_ite = minimi_(eje2_func, None, X0, tol, itmax)
     print(cant_ite)
 
     return param, param_eval
