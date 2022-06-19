f26aca19d659f4552c8c14123919711e84978299
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-06-19 16:20:30 -0300

    add ejer2 to entregable

--- TP5/ENTREGABLE/temperamental.py
+++ TP5/ENTREGABLE/temperamental.py
@@ -86,3 +86,50 @@ def minimi(func,grad,xo,tol,itmax):
                     X,f = evaluate_and_order(X,func)
                     S = X[0:n].sum(axis=0)
     return X[o],f[o],k
+
+import pandas as pd
+
+a = 1
+b = 1
+c = 1
+T1 = 1
+T2 = 1
+
+ti = []
+yi = []
+N = 0
+
+# Coleccion de puntos iniciales a b c T1 T2
+X0 = np.array([10,10,10,11,11])
+
+tol = 1e-12
+itmax = 1000
+
+parameter = np.array([a,b,c,T1,T2])
+
+def read_temp_file():
+    df = pd.read_csv('temp.txt',header=None,names=['ti','yi'],sep=' ')
+    ti = np.array(df['ti'].tolist())
+    yi = np.array(df['yi'].tolist())
+    N = len(ti)
+    return ti, yi, N
+
+def eje2_func(parameter):
+    global ti, yi, N
+
+    a = parameter[0]
+    b = parameter[1]
+    c = parameter[2]
+    T1 = parameter[3]
+    T2 = parameter[4]
+
+    aux = abs(yi-(a + b*np.cos(2*np.pi*ti/T1) + c*np.cos(2*np.pi*ti/T2)))**2
+    return aux.sum()/N
+
+def temperatura():
+    global ti, yi, N
+    ti, yi, N = read_temp_file()
+    param, param_eval, cant_ite = minimi(eje2_func, None, X0, tol, itmax)
+    #print(cant_ite)
+
+    return param, param_eval
