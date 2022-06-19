9b85bfcc3c57e71fd1253b9f76276008ead50e46
alero <amoldovan@itba.edu.ar>
2022-06-19 16:37:29 -0300

    COMENTADO

--- TP5/ENTREGABLE/temperamental.py
+++ TP5/ENTREGABLE/temperamental.py
@@ -1,23 +1,36 @@
 import numpy as np
 import random
 
+# Función auxiliar get_nearby_point:
+# Dado un punto n-dimensional, de forma aleatoria, consigue otro punto cercano
 def get_nearby_point(xo):
     x = np.array(xo,dtype=np.longdouble)
     for i in range(len(x)):
+        # Cada coordenada es alterada entre -5% y +5%
         x[i] = x[i] * (1 + (random.random()-0.5)/10)
     return x
 
+# Función auxiliar evaluate_and_order
+# Dado un arreglo de puntos y una función, evalúa la función en dichos
+# puntos, y los ordena según dichas evaluaciones
 def evaluate_and_order(X,func):
     f = np.array([func(xi) for xi in X])
     i = f.argsort()
     return X[i],f[i]
 
+# Función auxiliar replace_point
+# Dado un arreglo de puntos, una función, un arreglo de evaluaciones,
+# un punto nuevo, y un índice de posición, modifica el arreglo de puntos
+# y el de evaluaciones incluyendo el nuevo punto en la posición indicada
 def replace_point(X,f,func,R,pos,p,S):
     I = np.array([i for i in range(pos)] + [p] + [i for i in range(pos,p)],dtype=np.int64)
     X[p] = R; f[p] = func(R)
     X, f = X[I], f[I]
     return X, f, S - X[p] + R
 
+# Función auxiliar get_pos
+# Dado un arreglo ordenado y un escalar, devuelve el índice donde debería
+# colocarse dicho escalar en el arreglo.
 def get_pos(fR,f):
     for i,fi in enumerate(f):
         if fR < fi:
@@ -32,8 +45,9 @@ def get_pos(fR,f):
 #           xo:   Punto inicial para iterar
 #           tol:  Tolerancia máxima entre la función evaluada en el punto óptimo y el peor
 #           itmax:Numero máximo de iteraciones.
-#Devuelve:  t:     arreglo con los instantes de tiempo
-#           x:     aproximaciones numéricas a x (una fila por instante de tiempo)
+#Devuelve:  Xo: Punto óptimo final
+#           fo: Función evaluada en dicho punto
+#           k:  Número de iteraciones que llevó realizó
 def minimi(func,grad,xo,tol,itmax):
     random.seed() # Para conseguir n+1 puntos iniciales, se inicializa la librería random
 
@@ -42,49 +56,63 @@ def minimi(func,grad,xo,tol,itmax):
     b = n - 1   # Índice del punto bueno
     p = n       # Índice del punto peor
 
+    # Se consiguen los n+1 puntos iniciales
     X = np.array([get_nearby_point(xo) for i in range(n+1)])
+    # Se los ordena según el valor de f evaluada en dichos puntos
     X,f = evaluate_and_order(X,func)
-    S = X[0:n].sum(axis=0)
+    S = X[0:n].sum(axis=0) # Se calcula la suma de los primeros n
 
-    for k in range(itmax):
+    for k in range(itmax): # En cada iteración...
+        # ... si la distancia entre el punto óptimo y el peor es menor a la tolerancia,
+        # y además la diferencia entre la función evaluada en dichos puntos es también menor
+        # a la tolerancia:
         if np.linalg.norm(X[o]-X[p]) < tol and np.linalg.norm(f[o]-f[p]) < tol:
+            # Termina el algoritmo
             break
-        M = S / n
+        M = S / n   # Punto medio para calcular reflexiones
 
         # REFLEXIÓN
         R = 2*M - X[p]; fR = func(R)
-        if fR < f[b]:
-            if fR > f[o]:
-                i = get_pos(fR,f)
-                X,f,S = replace_point(X,f,func,R,i,p,S)
-            else:
+        if fR < f[b]: # Reflexión mejor que el bueno
+            if fR > f[o]: # Reflexión peor que el óptimo
+                # Me quedo con la reflexión
+                i = get_pos(fR,f) # Calculo su posición en el arreglo
+                X,f,S = replace_point(X,f,func,R,i,p,S) # Lo coloco, eliminando el peor
+
+            else: # Reflexión mejor que el óptimo
                 # EXPANSION
                 E = 3*M - 2*X[p]; fE = func(E)
-                if fE < f[o]:
-                    X,f,S = replace_point(X,f,func,E,o,p,S)
-                else:
-                    X,f,S = replace_point(X,f,func,R,o,p,S)
-        else:
-            if fR < f[p]:
-                i = get_pos(fR,f)
-                X,f,S = replace_point(X,f,func,R,i,p,S)
-            else:
+                if fE < f[o]: # Expansión mejor que el óptimo
+                    # Me quedo con la expansión
+                    X,f,S = replace_point(X,f,func,E,o,p,S) # La coloco, eliminando el peor
+                else: # Expansión peor que el óptimo
+                    # Me quedo con la reflexión
+                    X,f,S = replace_point(X,f,func,R,o,p,S) # La coloco, eliminando el peor
+        else: # Reflexión peor que el bueno
+            if fR < f[p]: # Reflexión mejor que el peor
+                # Me quedo con la reflexión
+                i = get_pos(fR,f) # Calculo su posición en el arreglo
+                X,f,S = replace_point(X,f,func,R,i,p,S) # Lo coloco, eliminando el peor
+            else: # Reflexión peor que el peor
                 # CONTRACCIÓN
                 C1, C2 = (R+M)/2, (X[p]+M)/2
                 fC1,fC2 = func(C1),func(C2)
+                # Calculo la mejor C
                 if fC1<fC2:
                     C,fC = C1,fC1
                 else:
                     C,fC = C2,fC2
-                if fC < f[p]:
-                    i = get_pos(fC,f)
-                    X,f,S = replace_point(X,f,func,C,i,p,S)
-                else:
+                if fC < f[p]: # Contracción mejor que el peor
+                    # Me quedo con la contracción
+                    i = get_pos(fC,f) # Calculo su posición en el arreglo
+                    X,f,S = replace_point(X,f,func,C,i,p,S) # Lo coloco, eliminando el peor
+                else: # Contracción peor que el peor
                     # ENCOGIMIENTO
-                    for i in range(1,n+1):
-                        X[i] = (X[i] + X[o])/2
+                    for i in range(1,n+1): # A todos los puntos que no sean el óptimo
+                        X[i] = (X[i] + X[o])/2 # Se reemplazan por uno más cercano al óptimo
+                    # Se los ordena según el valor de f evaluada en dichos puntos
                     X,f = evaluate_and_order(X,func)
-                    S = X[0:n].sum(axis=0)
+                    S = X[0:n].sum(axis=0) # Se calcula la suma de los primeros n
     return X[o],f[o],k
 
 import pandas as pd
